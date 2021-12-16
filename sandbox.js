4cc0c7cec783441c85c2705d912d8bad909d5a3a
Heba-Webdev23 <jtvonline@yahoo.com>
2021-12-16 03:36:47 +0300

    done with the starting phase of the game

---
+++ sandbox.js
@@ -0,0 +1,59 @@
+
+//A variable to keep track when to open and when to close the rules page
+
+let rulesCount = 0;
+
+//The game section
+
+let game = document.querySelector('.rock-paper-scissors ');
+
+//The title section
+
+let titleAndScore = document.querySelector('.title-score')
+
+//The rules section close button
+
+let closeRulesBtn = document.querySelector('.rules-close');
+
+//The rules section open button
+
+let openRulesBtn = document.querySelector('.readrules');
+
+//The rules section
+
+let rules = document.querySelector('.rules');
+rules.style.display = 'none';
+
+
+openRulesBtn.addEventListener('click', openThePage);
+
+function openThePage() {
+   if(rulesCount === 0) {
+    rulesCount++;
+    openRulesBtn.style.display = 'none';
+    document.body.style.overflow = 'hidden';
+    rules.style.display = '';
+    document.body.style.padding = '0';
+    game.style.display = 'none';
+    titleAndScore.style.display = 'none';
+   }
+
+}
+
+
+closeRulesBtn.addEventListener('click', closeThePage);
+
+function closeThePage() {
+
+    if(rulesCount == 1) {
+      document.body.style.padding = '2rem';
+      document.body.style.overflow = '';
+      rules.style.display = 'none';
+      game.style.display = '';
+      titleAndScore.style.display = '';
+      openRulesBtn.style.display = '';
+      game.style.display = '';
+      rulesCount--;
+    }
+}
+
