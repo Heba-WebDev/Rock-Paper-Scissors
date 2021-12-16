d29fe0874c1af3255a06a4fe04314b3f0ac93240
Heba-Webdev23 <jtvonline@yahoo.com>
2021-12-16 19:48:58 +0300

    added the pick section

--- sandbox.js
+++ sandbox.js
@@ -3,6 +3,10 @@
 
 let rulesCount = 0;
 
+//a variable to check if the player chose or not
+
+let playerChose = false;
+
 //The game section
 
 let game = document.querySelector('.rock-paper-scissors ');
@@ -24,6 +28,10 @@ let openRulesBtn = document.querySelector('.readrules');
 let rules = document.querySelector('.rules');
 rules.style.display = 'none';
 
+//The chosing section
+
+let playerVsHouse = document.querySelector('.playerVsHouse');
+
 
 openRulesBtn.addEventListener('click', openThePage);
 
@@ -35,6 +43,7 @@ function openThePage() {
     rules.style.display = '';
     document.body.style.padding = '0';
     game.style.display = 'none';
+    playerVsHouse.style.display = 'none';
     titleAndScore.style.display = 'none';
    }
 
@@ -53,7 +62,67 @@ function closeThePage() {
       titleAndScore.style.display = '';
       openRulesBtn.style.display = '';
       game.style.display = '';
+      
       rulesCount--;
     }
+
+    if(playerChose == true) {
+      playerVsHouse.style.display = '';
+      game.style.display = 'none';
+    } else {
+      playerVsHouse.style.display = 'none';
+      game.style.display = '';
+    }
 }
+
+
+
+
+
+
+
+//The varabile that will keep track of the choise of the player
+
+let thePlayerChose = '';
+
+//Paper
+
+let paper = document.querySelector('.paper');
+
+paper.addEventListener('click', playerChosePaper);
+
+function playerChosePaper() {
+  game.style.display = 'none';
+  thePlayerChose += 'paper';
+}
+
+//Rock
+
+let rock = document.querySelector('.rock');
+
+rock.addEventListener('click', playerChoseRock);
+
+function playerChoseRock() {
+  game.style.display = 'none';
+  thePlayerChose += 'rock';
+}
+
+
+//Scissor
+
+let scissor = document.querySelector('.scissor');
+
+
+scissor.addEventListener('click', playerChoseScissor);
+
+function playerChoseScissor() {
+  game.style.display = 'none';
+  thePlayerChose += 'scissor';
+}
+
+
+
+game.style.display = 'none';
+
+
 
